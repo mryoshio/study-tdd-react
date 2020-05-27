c4ea7a69fd04fe6421274da11c04c1849e962cf2
mryoshio <yoshiokaas@gmail.com>
2020-05-27 11:19:36 +0900

    fix: unicode in eslint

--- tdd-carousel/.eslintrc.js
+++ tdd-carousel/.eslintrc.js
@@ -12,7 +12,12 @@ module.exports = {
     node: true
   },
   rules: {
-    quotes: ['error',​'single', { avoidEscape: true​}],
-    'comma-dangle': ['error',​'always-multiline']
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline']
+  },
+  settings: {
+    react: {
+      version: '16.4.2'
+    }
   }
 }
