975b7fbaadb63eb638eb04699be8497be4ef6dc3
mryoshio <yoshiokaas@gmail.com>
2020-06-06 14:33:53 +0900

    feat: add new props to carousel slide & test

--- tdd-carousel/src/CarouselSlide.js
+++ tdd-carousel/src/CarouselSlide.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const CarouselSlide = () => (
-    <figure>
-    <img />
-    <figcaption />
+const CarouselSlide = ({ imgUrl, description, attribution, ...rest}) => (
+    <figure {...rest}>
+    <img src={imgUrl} />
+    <figcaption>
+    <strong>{description}</strong> {attribution}
+    </figcaption>
     </figure>
 )
+
+CarouselSlide.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  description: PropTypes.node.isRequired,
+  attribution: PropTypes.node
+}
 
 export default CarouselSlide
--- tdd-carousel/src/tests/CarouselSlide.test.js
+++ tdd-carousel/src/tests/CarouselSlide.test.js
@@ -4,8 +4,13 @@ import CarouselSlide from '../CarouselSlide'
 
 describe('carousel slide', () => {
   let wrapper
+  let imgUrl = 'https://example.com/image.png'
+  let description = 'A beautiful image'
+  let attribution = 'trevor burnham'
   beforeEach(() => {
-    wrapper = shallow(<CarouselSlide />)
+    wrapper = shallow(
+        <CarouselSlide imgUrl={imgUrl} description='default image' />
+    )
   })
   it('renders a <figure>', () => {
     expect(wrapper.type()).toBe('figure')
@@ -13,6 +18,25 @@ describe('carousel slide', () => {
   it('renders a <img> and a <figcaption> as children', () => {
     expect(wrapper.childAt(0).type()).toBe('img')
     expect(wrapper.childAt(1).type()).toBe('figcaption')
+  })
+  it('passes `imgUrl` through to the <img>', () => {
+    wrapper.setProps({ imgUrl })
+    const img = wrapper.find('img')
+    expect(img.prop('src')).toBe(imgUrl)
+  })
+  it('uses description and attribution as <figcaption>', () => {
+    wrapper.setProps({ description, attribution })
+    expect(wrapper.find('figcaption').text()).toBe(`${description} ${attribution}`)
+    expect(wrapper.find('figcaption strong').text()).toBe(description)
+  })
+  it('passes other props throught the <figure>', () => {
+    const style ={}
+    const onClick = () => {}
+    const className = 'my-cr-slide'
+    wrapper.setProps({ style, onClick, className })
+    expect(wrapper.prop('style')).toBe(style)
+    expect(wrapper.prop('onClick')).toBe(onClick)
+    expect(wrapper.prop('className')).toBe(className)
   })
 })
      
