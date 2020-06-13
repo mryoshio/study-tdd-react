604d3dd4a375eaab8ab9ae2c2e43d9c2a0bd7a80
mryoshio <yoshiokaas@gmail.com>
2020-06-13 12:54:02 +0900

    feat: carousel slides to carousel component

--- tdd-carousel/src/Carousel.js
+++ tdd-carousel/src/Carousel.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import CarouselButton from './CarouselButton'
+import CarouselSlide from './CarouselSlide'
 
 class Carousel extends React.PureComponent {
   state = {
     slideIndex: 0
   }
   render() {
+    const { slides, ...rest } = this.props
     return (
-        <div>
+        <div {...rest}>
+        <CarouselSlide {...slides[this.state.slideIndex]} />
         <CarouselButton data-action='prev' onClick={this.handlePrevClick}>prev</CarouselButton>
         <CarouselButton data-action='next' onClick={this.handleNextClick}>next</CarouselButton>
         </div>
     )
   }
   handlePrevClick = () => {
+    const { slides } = this.props
     this.setState(({ slideIndex }) => ({
-      slideIndex: slideIndex - 1
+      slideIndex: (slideIndex + slides.length - 1) % slides.length
     }))
   }
   handleNextClick = () => {
+    const { slides } = this.props
     this.setState(({ slideIndex }) => ({
-      slideIndex: slideIndex + 1
+      slideIndex: (slideIndex + 1) % slides.length
     }))
   }
 }
--- tdd-carousel/src/tests/Carousel.test.js
+++ tdd-carousel/src/tests/Carousel.test.js
@@ -2,11 +2,31 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Carousel from '../Carousel'
 import CarouselButton from '../CarouselButton'
+import CarouselSlide from '../CarouselSlide'
 
 describe('carousel', () => {
   let wrapper
+
+  const slides = [
+    {
+      imgUrl: 'https://example.com/slide1.png',
+      description: 'slide 1',
+      attribution: 'uno pizza',
+    },
+    {
+      imgUrl: 'https://example.com/slide2.png',
+      description: 'slide 2',
+      attribution: 'dos equis',
+    },
+    {
+      imgUrl: 'https://example.com/slide3.png',
+      description: 'slide 3',
+      attribution: 'three amigos',
+    },
+  ]
+
   beforeEach(() => {
-    wrapper = shallow(<Carousel />)
+    wrapper = shallow(<Carousel slides={slides} />)
   })
 
   it('renders a <div>', () => {
@@ -25,7 +45,6 @@ describe('carousel', () => {
         .prop('children')
     ).toBe('prev')
   })
-
   it('renders a carousel button with `next`', () => {
     expect(
       wrapper
@@ -35,15 +54,46 @@ describe('carousel', () => {
     ).toBe('next')
   })
 
-  it('decrements slideIndex when prev clicked', () => {
-    wrapper.setState({ slideIndex: 1 })
-    wrapper.find("[data-action='prev']").simulate('click')
-    expect(wrapper.state('slideIndex')).toBe(0)
+  describe('with a middle slide selected', () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: 1 })
+    })
+    it('decrements slideIndex when prev clicked', () => {
+      wrapper.find("[data-action='prev']").simulate('click')
+      expect(wrapper.state('slideIndex')).toBe(0)
+    })
+
+    it('increments slideIndex when next clicked', () => {
+      wrapper.find("[data-action='next']").simulate('click')
+      expect(wrapper.state('slideIndex')).toBe(2)
+    })
   })
 
-  it('increments slideIndex when prev clicked', () => {
-    wrapper.setState({ slideIndex: 0 })
-    wrapper.find("[data-action='next']").simulate('click')
-    expect(wrapper.state('slideIndex')).toBe(1)
+  describe('with a first slide selected', () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: 0 })
+    })
+    it('renders the current slide as a carousel slide', () => {
+      let attribution = wrapper.find(CarouselSlide).props().attribution
+      expect(attribution).toEqual(slides[0].attribution)
+    })
+    it('decrements slideIndex when prev clicked', () => {
+      wrapper.find("[data-action='prev']").simulate('click')
+      expect(wrapper.state('slideIndex')).toBe(slides.length - 1)
+    })
+  })
+
+  describe('with a last slide selected', () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: slides.length - 1 })
+    })
+    it('renders the current slide as a carousel slide', () => {
+      let attribution = wrapper.find(CarouselSlide).props().attribution
+      expect(attribution).toEqual(slides[slides.length-1].attribution)
+    })
+    it('decrements slideIndex when next clicked', () => {
+      wrapper.find("[data-action='next']").simulate('click')
+      expect(wrapper.state('slideIndex')).toBe(0)
+    })
   })
 })
