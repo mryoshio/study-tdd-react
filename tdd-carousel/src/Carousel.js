901052bd578fe67a6d7c3b7fb0772b6e65b0748c
mryoshio <yoshiokaas@gmail.com>
2020-06-27 15:19:28 +0900

    feat: styled component for image

--- tdd-carousel/src/Carousel.js
+++ tdd-carousel/src/Carousel.js
@@ -5,8 +5,13 @@ import CarouselSlide from './CarouselSlide';
 
 class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
+  };
+
+  static defaultProps = {
+    defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
   };
 
   state = {
@@ -14,10 +19,10 @@ class Carousel extends React.PureComponent {
   };
 
   render() {
-    const { slides, ...rest } = this.props;
+    const { defaultImgHeight, slides, ...rest } = this.props;
     return (
       <div {...rest}>
-        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselSlide {...slides[this.state.slideIndex]} imgHeight={defaultImgHeight} />
         <CarouselButton data-action="prev" onClick={this.handlePrevClick}>
           prev
         </CarouselButton>
